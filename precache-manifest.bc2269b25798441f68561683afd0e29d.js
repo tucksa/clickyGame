02745e3c549b7f40b840b3239ebc905ed24b13c8
Monty <tuckerjsarah@gmail.com>
2019-04-28 16:10:55 -0400

    Updates

---
+++ precache-manifest.bc2269b25798441f68561683afd0e29d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfdc2e3c53affc8eac055d278ec045a4",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "e3fc111f50aac7e20746",
+    "url": "/clickyGame/static/css/main.3229d49f.chunk.css"
+  },
+  {
+    "revision": "61433e735fbb2d010e13",
+    "url": "/clickyGame/static/js/2.b7f52e28.chunk.js"
+  },
+  {
+    "revision": "e3fc111f50aac7e20746",
+    "url": "/clickyGame/static/js/main.88b21f8c.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  },
+  {
+    "revision": "b6373fdcda043be16ed46ae1cb2e9180",
+    "url": "/clickyGame/static/media/70sbkgrnd.b6373fdc.jpg"
+  },
+  {
+    "revision": "472486bc26a777c101008fe117f8a352",
+    "url": "/clickyGame/static/media/light-veneer.472486bc.png"
+  }
+]);
